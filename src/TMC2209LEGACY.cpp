03a5d8c360126e34e49079bad93c33e1c3f69f56
jnealo32 <joebneal1@gmail.com>
2025-05-03 23:21:24 -0600

    Version 1.0.1

--- src/TMC2209LEGACY.cpp
+++ src/TMC2209LEGACY.cpp
@@ -1,6 +1,6 @@
 /*
  * Filename: TMC2209LEGACY.cpp
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Joseph Neal
  * Date: 2025-03-21
  * Description: Basic Arduino library for driving the TMC2209 operated in Legacy Mode. 
